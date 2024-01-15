e4101e2580e508f7b7a10d6ec5ac38dc4d09989c
Ksenia Krivobokova <kseniadovydenko@yandex.ru>
2024-01-15 15:06:37 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = Math.pow(b, 2)-4 * a * c;
+  console.log(d);
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+      let x = (-b / (2 * a));
+      arr.push(x);
+      return arr;
+  } else if (d > 0) {
+      let x1 = (-b + Math.sqrt(d)) / (2 * a);
+      let x2 = (-b - Math.sqrt(d) ) / (2 * a);
+      arr.push(x1);
+      arr.push(x2);
+      return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
